b315d8ecfcf2149b9d5c3d6619147597687f8a88
blackheart-sckech <tchouananan74@gmail.com>
2025-03-19 12:12:54 +0100

    .

--- database/seeders/TopographyProjectSeeder.php
+++ database/seeders/TopographyProjectSeeder.php
@@ -11,36 +11,36 @@ class TopographyProjectSeeder extends Seeder
     {
         $projects = [
             [
-                'title' => 'Bornage terrain résidentiel Cocody',
+                'title' => 'Bornage terrain résidentiel Bastos',
                 'type' => 'bornage',
-                'description' => 'Délimitation précise d\'un terrain de 1000m² avec pose de bornes officielles',
+                'description' => 'Délimitation précise d\'un terrain de 1000m² avec pose de bornes officielles dans le quartier diplomatique',
                 'images' => [
                     'images/topography/topo11.jpg',
                     'images/topography/topo11.jpg'
                 ],
-                'location' => 'Cocody, Abidjan',
+                'location' => 'Bastos, Yaoundé',
                 'completion_date' => '2024-02-15'
             ],
             [
-                'title' => 'Levé topographique Riviera',
+                'title' => 'Levé topographique Bonanjo',
                 'type' => 'leve',
-                'description' => 'Cartographie détaillée d\'un terrain de 2500m² pour un projet immobilier',
+                'description' => 'Cartographie détaillée d\'un terrain de 2500m² pour un projet immobilier dans le quartier administratif',
                 'images' => [
                     'images/topography/topo10.jpg',
                     'images/topography/topo10.jpg'
                 ],
-                'location' => 'Riviera, Abidjan',
+                'location' => 'Bonanjo, Douala',
                 'completion_date' => '2024-01-20'
             ],
             [
-                'title' => 'Nivellement Yopougon',
+                'title' => 'Nivellement Biyem-Assi',
                 'type' => 'nivellement',
-                'description' => 'Étude de nivellement pour l\'implantation d\'un complexe commercial',
+                'description' => 'Étude de nivellement pour l\'implantation d\'un complexe commercial dans une zone en développement',
                 'images' => [
                     'images/topography/topo9.jpg',
                     'images/topography/topo9.jpg'
                 ],
-                'location' => 'Yopougon, Abidjan',
+                'location' => 'Biyem-Assi, Yaoundé',
                 'completion_date' => '2024-03-01'
             ]
         ];
